596c7a38ed84f4a6743c7de6d8557479747df57b
Pablo Gonzalez <pablo.gonzalez7@udea.edu.co>
2026-04-22 00:27:37 -0500

    Clase jugador

---
+++ UdeAWorldCup/jugador.cpp
@@ -0,0 +1,161 @@
+#include "Jugador.h"
+
+Jugador::Jugador()
+{
+    nombre = "";
+    apellido = "";
+    numeroCamiseta = 0;
+
+    partidosJugados = 0;
+    goles = 0;
+    minutos = 0;
+    asistencias = 0;
+    amarillas = 0;
+    rojas = 0;
+    faltas = 0;
+}
+
+Jugador::Jugador(const string& nombre, const string& apellido, int numeroCamiseta)
+{
+    this->nombre = nombre;
+    this->apellido = apellido;
+    this->numeroCamiseta = numeroCamiseta;
+
+    partidosJugados = 0;
+    goles = 0;
+    minutos = 0;
+    asistencias = 0;
+    amarillas = 0;
+    rojas = 0;
+    faltas = 0;
+}
+
+Jugador::Jugador(const Jugador& otro)
+{
+    nombre = otro.nombre;
+    apellido = otro.apellido;
+    numeroCamiseta = otro.numeroCamiseta;
+
+    partidosJugados = otro.partidosJugados;
+    goles = otro.goles;
+    minutos = otro.minutos;
+    asistencias = otro.asistencias;
+    amarillas = otro.amarillas;
+    rojas = otro.rojas;
+    faltas = otro.faltas;
+}
+
+string Jugador::getNombre() const
+{
+    return nombre;
+}
+
+string Jugador::getApellido() const
+{
+    return apellido;
+}
+
+int Jugador::getNumeroCamiseta() const
+{
+    return numeroCamiseta;
+}
+
+int Jugador::getPartidosJugados() const
+{
+    return partidosJugados;
+}
+
+int Jugador::getGoles() const
+{
+    return goles;
+}
+
+int Jugador::getMinutos() const
+{
+    return minutos;
+}
+
+int Jugador::getAsistencias() const
+{
+    return asistencias;
+}
+
+int Jugador::getAmarillas() const
+{
+    return amarillas;
+}
+
+int Jugador::getRojas() const
+{
+    return rojas;
+}
+
+int Jugador::getFaltas() const
+{
+    return faltas;
+}
+
+void Jugador::setGoles(int golesIniciales)
+{
+    if (golesIniciales >= 0)
+    {
+        goles = golesIniciales;
+    }
+}
+
+void Jugador::registrarPartido(int minutosJugados)
+{
+    if (minutosJugados > 0)
+    {
+        partidosJugados++;
+        minutos += minutosJugados;
+    }
+}
+
+void Jugador::sumarGol()
+{
+    goles++;
+}
+
+void Jugador::sumarAsistencia()
+{
+    asistencias++;
+}
+
+void Jugador::sumarAmarilla()
+{
+    amarillas++;
+}
+
+void Jugador::sumarRoja()
+{
+    rojas++;
+}
+
+void Jugador::sumarFalta()
+{
+    faltas++;
+}
+
+bool Jugador::operator==(const Jugador& otro) const
+{
+    return nombre == otro.nombre &&
+           apellido == otro.apellido &&
+           numeroCamiseta == otro.numeroCamiseta;
+}
+
+ostream& operator<<(ostream& os, const Jugador& j)
+{
+    os << "Nombre: " << j.nombre
+       << " " << j.apellido
+       << " | Camiseta: " << j.numeroCamiseta
+       << " | PJ: " << j.partidosJugados
+       << " | Goles: " << j.goles
+       << " | Minutos: " << j.minutos
+       << " | Asistencias: " << j.asistencias
+       << " | Amarillas: " << j.amarillas
+       << " | Rojas: " << j.rojas
+       << " | Faltas: " << j.faltas;
+
+    return os;
+}
---
+++ UdeAWorldCup/jugador.h
@@ -0,0 +1,54 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Jugador
+{
+private:
+    string nombre;
+    string apellido;
+    int numeroCamiseta;
+
+    int partidosJugados;
+    int goles;
+    int minutos;
+    int asistencias;
+    int amarillas;
+    int rojas;
+    int faltas;
+
+public:
+    Jugador();
+    Jugador(const string& nombre, const string& apellido, int numeroCamiseta);
+    Jugador(const Jugador& otro);
+
+    string getNombre() const;
+    string getApellido() const;
+    int getNumeroCamiseta() const;
+    int getPartidosJugados() const;
+    int getGoles() const;
+    int getMinutos() const;
+    int getAsistencias() const;
+    int getAmarillas() const;
+    int getRojas() const;
+    int getFaltas() const;
+
+    void setGoles(int golesIniciales);
+
+    void registrarPartido(int minutosJugados);
+    void sumarGol();
+    void sumarAsistencia();
+    void sumarAmarilla();
+    void sumarRoja();
+    void sumarFalta();
+
+    bool operator==(const Jugador& otro) const;
+
+    friend ostream& operator<<(ostream& os, const Jugador& j);
+};
+
+#endif
